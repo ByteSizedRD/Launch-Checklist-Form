ba173f2612796b419cc9a0e7840cc66e77a27016
ByteSizedRD <56002023+ByteSizedRD@users.noreply.github.com>
2019-10-17 15:17:26 -0500

    Verification Code JS

--- script.js
+++ script.js
@@ -11,3 +11,38 @@
 </ol>
 <img src="${}">
 */
+
+
+window.addEventListener('load', function() {
+   let form = document.querySelector("form");
+   form.addEventListener('submit', function(event) {
+     
+ 
+
+      let pilotNameInput = document.querySelector("input[name=pilotName");
+      let coPilotNameInput = document.querySelector("input[name=copilotName");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel");
+      let cargoMassInput = document.querySelector("input[name = cargoMass");
+      if (pilotNameInput.value === "" || coPilotNameInput === "" || fuelLevelInput === "" || cargoMassInput === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      } else if {        
+         (isNaN(pilotNameInput.value) || isNaN(coPilotNameInput))
+         alert("Please enter correct input value")
+         event.preventDefault();
+
+      let nameUpdate =
+      `<div  id="faultyItems">
+      <ol>
+          <li id="pilotStatus">${pilotNameInput} Ready</li>
+          <li id="copilotStatus">${copilotNameInput} Ready</li>
+          <li id="fuelStatus">Fuel level high enough for launch</li>
+          <li id="cargoStatus">Cargo mass low enough for launch</li>
+      </ol>
+       </div>`
+
+
+      }
+
+   })
+})
